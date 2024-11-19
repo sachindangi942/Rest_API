5bbc1884ce33cc35bb83b1874bab9ff131d61109
sachindangi942 <sachindangi942@gmail.com>
2024-11-19 14:43:02 +0530

    this is restful api with authentication

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const App = express();
 require("dotenv").config();
-const registration = require("./routes/routers")
+const Router = require("./routes/routers")
 const {db_connection} = require("./databases/mongoose")
 
 const port = process.env.PORT || 9000 || 9001
@@ -10,7 +10,7 @@ db_connection();
 
 App.use(express.json());
 
-App.use("/user",registration);
+App.use("/user",Router);
 
 App.listen(port,()=>{console.log(`server is running on port ${port}`)})
 
